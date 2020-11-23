812161d308413a065cc25d41067eb05cbb59e9b5
EliseoLux <68084030+EliseoLux@users.noreply.github.com>
2020-11-22 23:02:44 -0600

    Final

---
+++ uP/FF_D_1.v
@@ -0,0 +1,12 @@
+// Juan Eliseo Lux Cajas
+// ProyectoFinal-Electronica1
+
+//FF de 1 bit
+module FF_D_1(input clk, reset, enabled, D, output reg Q);
+
+    always @ (posedge clk, posedge reset)
+        if (reset)
+            Q = 1'b0;
+        else if (enabled)
+            Q = D;
+endmodule
---
+++ uP/buffer.v
@@ -0,0 +1,9 @@
+// Juan Eliseo Lux Cajas
+// ProyectoFinal-Electronica1
+
+module buffer(input enabled, input [3:0] A, output [3:0] Y);
+
+    // asignacion para la salida del buffer
+    assign Y = enabled ? A : 4'bz;
+
+endmodule
